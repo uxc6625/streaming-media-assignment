bbd2d39f435b9f7460210a58265f90839ca2c26b
Ulijah Charles <uxc6625@rit.edu>
2019-02-03 22:41:33 -0500

    intial upload

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponse.js');
+const mediaHandler = require('./mediaResponse.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/party.mp4':
+      mediaHandler.getParty(request, response);
+      break;
+    case '/page2':
+      mediaHandler.getBling(request, response);
+      break;
+    case '/page3':
+      mediaHandler.getBird(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
